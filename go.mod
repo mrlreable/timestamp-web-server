ee0d32561de6096b6d5e8564ec3b7dc0468add6a
Levente <lang.levente99@gmail.com>
2024-11-19 17:57:04 +0100

    ENHANCEMENT module init, main_test.go, main.go added

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mrlreable/timestamp-web-server
+
+go 1.23.1
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestFetchTime(t *testing.T) {
+	panic("Not implemented")
+}
+
+func TestSaveTime(t *testing.T) {
+	panic("Not implemented")
+}
